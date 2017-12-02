5035c5ae5bc1269d48f55305a806138c46df76a4
Jibran <pictojam@gmail.com>
2017-12-02 13:00:18 -0600

    new updates

--- src/app/post/Post.js
+++ src/app/post/Post.js
@@ -1,6 +1,5 @@
 import React from "react";
 
-
 export class Post extends React.Component {
   constructor(props) {
   	 super(props);
@@ -9,14 +8,17 @@ export class Post extends React.Component {
       comments:[],
       currentInput: null,
      	parentComment: [],
-      childComment:[]
+      childComment:[],
+      displayReplyInput: false,
+      replyObj: null
      };
      this.formSubmit = this.formSubmit.bind(this);
      this.inputChange = this.inputChange.bind(this);
-     this.addReply = this.addReply.bind(this);
+     this.replyInput = this.replyInput.bind(this);
+     this.postReply = this.postReply.bind(this);
   }
 
-   componentDidMount(){
+  componentDidMount(){
            console.log(`compontent mounted`, this.state);
            var tab = parseInt(this.props.routeParams.tab);
            fetch(`http://localhost:9001/posts`)
@@ -30,7 +32,6 @@ export class Post extends React.Component {
          	.then(result => result.json())
          	.then(data => {
        		     var commentsArray = data.filter(function(elem, i, array) {
-                  // console.table(array)
                   return elem.postId == tab;
                });
 
@@ -41,12 +42,11 @@ export class Post extends React.Component {
                         this.state.childComment.push(sorted);
                      }
                 });
-
                 var comments = this.state.parentComment.map((parent) => {
                    var childComment = this.state.childComment.map((child)=> {
                       if (child.parent_id == parent.id){
                         return (
-                          <div className="row" id={ "comment-" + child.parent_id } key={ child.id }>
+                          <div className="item" id={ "comment-" + child.parent_id } key={ child.id }>
                              <div className="col-sm-2">
                                 <div className="thumbnail">
                                    <img className="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png" />
@@ -68,28 +68,21 @@ export class Post extends React.Component {
                       }
                    });
 
+
+                     // this.setState({replyInput: replyInput});
+                     // console.log(this.state.replyInput);
+
                    return (
-                      <div className="row" id={ "comment-" + parent.id } key={ parent.id }>
-                         <div className="col-sm-2">
-                            <div className="thumbnail">
-                               <img className="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-                            </div>
 
-                         </div>
-                         <div className="col-sm-10">
-                            <div className="panel panel-default">
-                               <div className="panel-heading">
-                                  <strong>{parent.user}</strong> <span className="text-muted">commented on {parent.date}</span>
-                                  <button className="addReply btn btn-primary pull-right" onClick={this.addReply}>reply</button>
-                               </div>
-                               <div className="panel-body">
-                                  <div className="parent-comment"> "{parent.content}" </div>
-
-                                  <div className="child-comment">{ childComment }</div>
-                               </div>
-                            </div>
-                         </div>
-                      </div>
+                     <div className="panel-default">
+                       <div className="panel-heading">
+                          <strong>{parent.user}</strong> <span className="text-muted">commented on {parent.date}</span>
+                       </div>
+                       <div className="panel-body">
+                          <div className="parent-comment"> "{parent.content}" </div>
+                          <div className="child-comment">{ childComment }</div>
+                       </div>
+                    </div>
 
                    )
                 });
@@ -97,13 +90,88 @@ export class Post extends React.Component {
                 this.setState({comments: comments})
          	});
    }
-   addReply(){
-     console.log("add reply");
+
+
+   replyInput(e){
+      // console.log(this.state.childComment);
+
+      var today = new Date();
+      var dd = today.getDate();
+      var mm = today.getMonth()+1; //January is 0!
+      var yyyy = today.getFullYear();
+
+      if(dd<10) { dd = '0'+dd}
+      if(mm<10) {mm = '0'+mm}
+      today = yyyy + '-' + mm + '-' + dd;
+
+      var value = e.target.value;
+      var id =  parseInt(e.target.id + 1);
+      var postID = parseInt(this.props.routeParams.tab);
+      var user = "User 1";
+      var date = new Date();
+      var key = date;
+      var obj = {id: key, content: value, date: today, parent_id: id, postId: postID, user: user}
+      this.setState({replyObj: obj});
+
    }
+
+   postReply(){
+     this.state.childComment.push(this.state.replyObj);
+     console.table(this.state.childComment);
+
+
+     var comments = this.state.parentComment.map((parent) => {
+        var childComment = this.state.childComment.map((child)=> {
+           if (child.parent_id == parent.id){
+             return (
+               <div className="item" id={ "comment-" + child.parent_id } key={ child.id }>
+                  <div className="col-sm-2">
+                     <div className="thumbnail">
+                        <img className="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+                     </div>
+
+                  </div>
+                  <div className="col-sm-10">
+                     <div className="panel panel-default">
+                        <div className="panel-heading">
+                           <strong>{child.user}</strong> <span className="text-muted">commented on {child.date}</span>
+                        </div>
+                        <div className="panel-body">
+                           <div className="parent-comment"> "{child.content}" </div>
+                        </div>
+                     </div>
+                  </div>
+               </div>
+             )
+           }
+        });
+
+
+          // this.setState({replyInput: replyInput});
+          // console.log(this.state.replyInput);
+
+        return (
+
+          <div className="panel-default">
+            <div className="panel-heading">
+               <strong>{parent.user}</strong> <span className="text-muted">commented on {parent.date}</span>
+            </div>
+            <div className="panel-body">
+               <div className="parent-comment"> "{parent.content}" </div>
+               <div className="child-comment">{ childComment }</div>
+            </div>
+         </div>
+
+        )
+     });
+     console.log(comments);
+     this.setState({comments: comments})
+   }
+
+
    inputChange(e){
      this.setState({ currentInput: e.target.value });
      // console.log(e.target.value, this.state.currentInput);
-
    }
    formSubmit(e){
      e.preventDefault();
@@ -119,27 +187,26 @@ export class Post extends React.Component {
      var date = new Date();
      var key = date;
      var newComment =
-           <div className="row" id={ "comment-" + key} key={ key }>
-              <div className="col-sm-2">
-                 <div className="thumbnail">
-                    <img className="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png" />
-                 </div>
+         <div className="item" id={ "comment-" + key} key={ key }>
+            <div className="col-sm-2">
+               <div className="thumbnail">
+                  <img className="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+               </div>
 
-              </div>
-              <div className="col-sm-10">
-                 <div className="panel panel-default">
-                    <div className="panel-heading">
-                       <strong>User 1</strong> <span className="text-muted">commented on {today}</span>
-                       <button className="addReply btn btn-primary pull-right" onClick={this.addReply}>reply</button>
-                    </div>
-                    <div className="panel-body">
-                      <div className="parent-comment"> "{this.state.currentInput}" </div>
-                    </div>
-                 </div>
-              </div>
-           </div>
+            </div>
+            <div className="col-sm-10">
+               <div className="panel panel-default">
+                  <div className="panel-heading">
+                     <strong>User 1</strong> <span className="text-muted">commented on {today}</span>
+                  </div>
+                  <div className="panel-body">
+                    <div className="parent-comment"> "{this.state.currentInput}" </div>
+                  </div>
+               </div>
+            </div>
+         </div>
      var currentComments = this.state.comments;
-     console.log(this.state.currentInput);
+     // console.log(this.state.currentInput);
      if (this.state.currentInput != null){
        currentComments.push(newComment);
        this.setState({comments: currentComments});
@@ -148,8 +215,6 @@ export class Post extends React.Component {
 
    }
 
-
-
   render(){
   	var body;
   	var __html;
@@ -157,6 +222,31 @@ export class Post extends React.Component {
     if(this.state.post !== null){
     	__html = this.state.post.content;
     }
+    // console.log(this.state.comments);
+
+
+    var comments  = this.state.comments;
+    var commentsDom = comments.map((elem, index) => {
+       return (
+         <div className="row" id={ "comment-" + index } key={ index }>
+            <div className="col-sm-2">
+               <div className="thumbnail">
+                  <img className="img-responsive user-photo" src="https://ssl.gstatic.com/accounts/ui/avatar_2x.png" />
+               </div>
+            </div>
+            <div className="col-sm-10">
+              <div className="panel panel-default">
+                      {elem}
+                      <div className="form-group panel-body">
+                          <input type="text" onChange={this.replyInput} className="form-control" id={index} placeholder="Your Reply" />
+                          <button name="reply" onClick={this.postReply} className="btn btn-primary" type="submit"> Post </button>
+                      </div>
+              </div>
+            </div>
+         </div>
+       )
+
+    })
     return(
         <div>
             {
@@ -165,19 +255,20 @@ export class Post extends React.Component {
    				       <h1 className="display-3"> {this.state.post.title}</h1>
 	               <span className="author small">{this.state.post.author} <br /> <i className="blockquote-footer"> {this.state.post.publish_date} </i></span>
                  <span className="content lead container" dangerouslySetInnerHTML={{ __html }} />
-
                </div>
             }
-            <div className="comments">
-             	     {this.state.comments}
-            </div>
+            <div className="comments">{commentsDom}</div>
             <div className="addComment">
                 <form onSubmit={this.formSubmit}>
                   <textarea name="addComment" ref="commentBox" className="lead" onKeyUp={this.inputChange} ></textarea>
-                  <button name="submit" type="submit"> Post </button>
+                  <button name="submit" className="btn btn-primary" type="submit"> Post </button>
                 </form>
             </div>
         </div>
     )
   }
+
+
+
+
 }
